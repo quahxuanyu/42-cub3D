5d36d33d70dcebb8bf75abc11f148c9f3d01100c
Quah XuanYu <xuanthegreat@gmail.com>
2024-12-12 14:27:12 +0800

    added partial code for sample raycast

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: xquah <xquah@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/06 13:17:37 by xquah             #+#    #+#             */
-/*   Updated: 2024/12/06 13:19:02 by xquah            ###   ########.fr       */
+/*   Updated: 2024/12/12 14:13:28 by xquah            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,11 @@
 
 #include "../minilibx-linux/mlx.h"
 #include <math.h>
+#include <stdio.h> 
+#include <stdbool.h>
+#define mapWidth 24
+#define mapHeight 24
+#define screenWidth 640
+#define screenHeight 480
 
 #endif
