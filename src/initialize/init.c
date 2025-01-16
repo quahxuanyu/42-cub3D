c29f6af4a82b5f11437685ceabf6629d3ea3a6d6
winniehhy <yoyowinnie3@gmail.com>
2025-01-17 00:21:55 +0800

    need to beautify the map

--- src/initialize/init.c
+++ src/initialize/init.c
@@ -6,7 +6,7 @@
 /*   By: hheng < hheng@student.42kl.edu.my>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/01 15:55:12 by hheng             #+#    #+#             */
-/*   Updated: 2025/01/16 17:00:42 by hheng            ###   ########.fr       */
+/*   Updated: 2025/01/17 00:20:34 by hheng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,29 +20,14 @@
 */
 void init_all(t_game *game, char *map_file, char *texture_file)
 {
-    printf("Debug: Initializing game\n");
     memset(game, 0, sizeof(t_game));  // Initialize all to 0 first
-    printf("Debug: Game structure initialized to zero\n");
 
     init_player(&game->player);
-    printf("Debug: Player initialized\n");
-
     init_mlx(game);
-    printf("Debug: MLX initialized\n");
-
     init_texture(game);
-    printf("Debug: Textures initialized\n");
     
     // Initialize map_data
     game->map_data.map = NULL;
     game->map_data.width = 0;
     game->map_data.height = 0;
-    printf("Debug: Map data initialized\n");
-    
-    // if (!game->map_data.map && map_file)  // Only try to load if we have a file
-    // {
-    //     printf("Error: Map not properly initialized\n");
-    //     exit(EXIT_FAILURE);
-    // }    
-    printf("Debug: Initialization complete\n");
 }
